53c60449fa79cabb4b23582bf38d2da9d705932a
Roger Keller <rokeller@cymbeline.ch>
2022-12-10 20:01:43 +0100

    Add UTC clock too.

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -2,15 +2,26 @@ import { useEffect, useState } from 'react';
 import { formatNumber } from '../utils';
 
 function Clock() {
-    const [time, setTime] = useState('loading ...');
+    const [localTime, setLocalTime] = useState('loading ...');
+    const [utcTime, setUtcTime] = useState('loading ...');
+
+    function updateTimes() {
+        setLocalTime(getLocalTime());
+        setUtcTime(getUtcTime());
+    }
 
     useEffect(() => {
-        setTime(getLocalTime());
-        const interval = setInterval(() => setTime(getLocalTime()), 1000);
+        updateTimes();
+        const interval = setInterval(updateTimes, 499);
         return () => clearInterval(interval!);
     }, []);
 
-    return <div className='clock'>{time}</div>;
+    return <>
+        <div className='row sep-v'>utc time</div>
+        <div className='clock'>{utcTime}</div>
+        <div className='row sep-v'>local time</div>
+        <div className='clock'>{localTime}</div>
+    </>;
 }
 
 function getLocalTime() {
@@ -19,6 +30,14 @@ function getLocalTime() {
     return formatNumber(timestamp.getHours(), 2) + ':' +
         formatNumber(timestamp.getMinutes(), 2) + ':' +
         formatNumber(timestamp.getSeconds(), 2);
+}
+
+function getUtcTime() {
+    const timestamp = new Date();
+
+    return formatNumber(timestamp.getUTCHours(), 2) + ':' +
+        formatNumber(timestamp.getUTCMinutes(), 2) + ':' +
+        formatNumber(timestamp.getUTCSeconds(), 2);
 }
 
 export default Clock;
